c73098ee08dfc8b6becb58f27fed822966e3e86c
somaia96 <somaia96.sh@gmail.com>
2024-09-04 06:51:33 +0300

    Add Loading Page

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className="text-center text-3xl my-10 text-gray-500 font-[800]">
+            <h2>Loading <i className="loading fa-solid fa-spinner"></i></h2>
+        </div>
+    )
+}
+
+export default Loading
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,14 +5,19 @@ import {
 } from "react-router-dom";
 import PageNotFound from "../pages/PageNotFound";
 import Layout from "../pages/Layout";
-import React from "react";
+import React, { Suspense } from "react";
+import Loading from "../components/Loading";
 const Home = React.lazy(() => import("../pages/Home"));
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<Layout />} errorElement={<PageNotFound />} >
-        <Route index element={<Home />} />
+        <Route index element={<>
+          <Suspense fallback={<Loading />}>
+            <Home />
+          </Suspense>
+        </>} />
       </Route>
       <Route path="*" element={<PageNotFound />} />
     </>
