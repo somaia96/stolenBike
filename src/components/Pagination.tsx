36d8d8672e94198a0f8579cdd6de9a46a6493a10
somaia96 <somaia96.sh@gmail.com>
2024-09-04 15:10:59 +0300

    pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,58 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, IconButton } from "@material-tailwind/react";
 import { ArrowRightIcon, ArrowLeftIcon } from "@heroicons/react/24/outline";
-interface IProps  {
-  setPage:(val:number)=>void;
+interface IProps {
+  setPage: (val: number | ((value:number)=>number)) => void;
+  count: number;
 }
 
-export default function Pagination({setPage}:IProps) {
+export default function Pagination({ setPage, count }: IProps) {
   const [active, setActive] = React.useState(1);
- 
-  const getItemProps = (index:number) =>
-    ({
-      variant: active === index ? "filled" : "text",
-      color: "gray",
-      onClick: () =>{
-         setActive(index)
-        setPage(index)
-        },
-      className: "rounded-full",
-    } as any);
- 
+  const getItemProps = (index: number) =>
+  ({
+    variant: active === index ? "filled" : "text",
+    color: "gray",
+    onClick: () => {
+      setActive(index)
+      setPage(index)
+    },
+    className: "rounded-full",
+  } as any);
+
   const next = () => {
-    if (active === 500) return;
- 
-    setActive(active + 1);
+    if (active === count) return;
+    setI((prev) => (prev + 1))
+    setActive((prev) => (prev + 1));
+    setPage((prev)=>(prev + 1))
+  
   };
- 
+
   const prev = () => {
-    if (active === 1) return;
- 
-    setActive(active - 1);
+    if (active === 1 || i == 1) return;
+    setI((prev) => (prev - 1))
+    setActive((prev) => (prev - 1));
+    setPage((prev)=>(prev - 1))
+
   };
- 
+  const [i, setI] = useState(1)
+
   return (
     <div className="flex items-center gap-4 justify-center mt-6">
       <Button
         variant="text"
         className="hidden md:flex items-center gap-2 rounded-full"
         onClick={prev}
-        disabled={active === 1}
+        disabled={active === 1 || i == 1}
       >
         <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" /> Previous
       </Button>
       <div className="flex items-center gap-2">
-        <IconButton {...getItemProps(1)}>1</IconButton>
-        <IconButton {...getItemProps(2)}>2</IconButton>
-        <IconButton {...getItemProps(3)}>3</IconButton>
-        <IconButton {...getItemProps(4)}>4</IconButton>
-        <IconButton {...getItemProps(5)}>5</IconButton>
+        <IconButton {...getItemProps(i)}>{i}</IconButton>
+        <IconButton {...getItemProps(i + 1)}>{i + 1}</IconButton>
+        <p> ... </p>
+        <IconButton {...getItemProps(count)}>{count}</IconButton>
       </div>
       <Button
         variant="text"
         className="hidden md:flex items-center gap-2 rounded-full"
         onClick={next}
-        disabled={active === 500}
+        disabled={active === count}
       >
         Next
         <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,18 +8,21 @@ const Home = () => {
   const [page, setPage] = useState(1)
   const [title, setTitle] = useState("")
   const [range, setRange] = useState("")
+  const [count, setCount] = useState(5000)
 
   useEffect(() => {
     fetch(`https://bikeindex.org/api/v3/search?query=${title}&page=${page}&per_page=10`)
       .then(data => data.json())
-      .then(data => setData(data.bikes))
+      .then(data => {
+        return setData(data.bikes)
+      })
       .catch(error => console.error('Error fetching data:', error));
-  }, [title,page])
+  }, [title, page])
 
   return <main className="container px-2 my-6">
     {
-      data.length ?
-        <> <div className="w-full flex gap-2 items-center justify-between max-h-24 h-20">
+      data.length ? <>
+        <div className="w-full flex gap-2 items-center justify-between max-h-24 h-20">
           <div className="w-full sm:w-11/12 flex flex-col justify-between">
             <input className="w-full h-10 outline-none border rounded-sm px-3 border-1 mb-2 border-gray-200"
               type="text"
@@ -41,12 +44,12 @@ const Home = () => {
             <i className="w-full fa-solid p-3 fa-search h-10"></i>
           </div>
         </div>
-          {data!.map((bike, i) => (
-            <Bike key={i} bike={bike} />
-          ))}
-        </> : <NoBike />
+        {data!.map((bike, i) => (
+          <Bike key={i} bike={bike} />
+        ))}
+      </> : <NoBike />
     }
-    <Pagination setPage={setPage} />
+    <Pagination count={count} setPage={setPage} />
   </main>
 }
 
