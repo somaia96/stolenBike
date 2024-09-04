70de50de473d9f63764a89f027f04c7021ee3579
somaia96 <somaia96.sh@gmail.com>
2024-09-04 08:19:15 +0300

    fix fontawesome

--- src/components/NoBike.tsx
+++ src/components/NoBike.tsx
@@ -2,7 +2,7 @@
 const NoBike = () => {
   return (
     <div className="flex justify-center gap-3 items-center my-6">
-      <h2>No Bikes Found </h2> <i className="fa-solid fa-none"></i>
+      <h2>No Bikes Found </h2> <i className="fa-solid fa-lg fa-circle-exclamation"></i>
     </div>
   )
 }
