dea78159a0bf715c434e60cdcb712157ac8f422c
somaia96 <somaia96.sh@gmail.com>
2024-09-04 07:03:15 +0300

    NoBike & Pagination & Search

---
+++ src/components/NoBike.tsx
@@ -0,0 +1,10 @@
+
+const NoBike = () => {
+  return (
+    <div className="flex justify-center gap-3 items-center my-6">
+      <h2>No Bikes Found </h2> <i className="fa-solid fa-none"></i>
+    </div>
+  )
+}
+
+export default NoBike
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,54 @@
 import { useEffect, useState } from "react"
 import Bike from "../components/Bike"
-const Home = () => {
-  const [data, setData] = useState<Bike[]>([])
+import Pagination from "../components/Pagination"
+import NoBike from "../components/NoBike"
+
+const Home = () => {const [data, setData] = useState<Bike[]>([])
   const [page, setPage] = useState(1)
+  const [title, setTitle] = useState("")
+  const [range, setRange] = useState("")
 
   useEffect(() => {
     fetch(`https://bikeindex.org/api/v3/search?page=${page}&per_page=10`)
       .then(data => data.json())
       .then(data => setData(data.bikes))
   }, [page])
+  useEffect(() => {
+    fetch(`https://bikeindex.org/api/v3/search?query=${title}&page=${page}&per_page=10`)
+      .then(data => data.json())
+      .then(data => setData(data.bikes))
+  }, [title])
   return <main className="container px-2 my-6">
+    {
+      data.length ?
+        <> <div className="w-full flex gap-2 items-center justify-between max-h-24 h-20">
+          <div className="w-11/12 flex flex-col justify-between">
+            <input className="w-full h-10 outline-none border rounded-sm px-3 border-1 mb-2 border-gray-200"
+              type="text"
+              placeholder="Search Bike Title"
+              value={title}
+              onChange={e => {
+                setPage(1)
+                return setTitle(e.target.value)
+              }}
+            />
+            <input className="w-full h-10 outline-none border rounded-sm px-3 border-1 border-gray-200"
+              type="text"
+              placeholder="Search By Date Range"
+              value={range}
+              onChange={e => setRange(e.target.value)}
+            />
+          </div>
+          <div className="flex justify-center text-sm items-center w-32 p-11 bg-blue-gray-50 rounded-sm h-full">
+            <i className="w-full fa-solid p-3 fa-search h-10"></i>
+          </div>
+        </div>
           {data!.map((bike, i) => (
               <Bike key={i} bike={bike} />
           ))}
+        </> : <NoBike />
+    }
+    <Pagination setPage={setPage} />
   </main>
 }
 
